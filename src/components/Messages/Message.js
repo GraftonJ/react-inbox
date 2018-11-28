957ecb4e4cfb657c703b64eadba9bd44b743c003
Josh <grafton.josh@gmail.com>
2018-11-28 16:36:48 -0700

    working star

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
 const Message = ({ message, onStar }) => (
-  <div class="row message read">
-    <div class="col-xs-1">
-      <div class="row">
-        <div class="col-xs-2">
+  <div className="row message read">
+    <div className="col-xs-1">
+      <div className="row">
+        <div className="col-xs-2">
           <input type="checkbox" />
         </div>
-        <div class="col-xs-2">
-          <i class={`star fa ${message.starred ? "fa-star" : "fa-star-o"}`} onClick={ () => {onStar(message.id)}}></i>
+        <div className="col-xs-2">
+          <i className={`star fa ${message.starred ? "fa-star" : "fa-star-o"}`} onClick={ () => {onStar(message.id)}}></i>
         </div>
       </div>
     </div>
-    <div class="col-xs-11">
+    <div className="col-xs-11">
       <a href="#">
         {message.subject}
       </a>
